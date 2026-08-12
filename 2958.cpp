9333944f6041be45e8633c05629ba46de0e73154
Tohid Kazi <155968885+Tohid-k-001@users.noreply.github.com>
2026-08-12 22:59:21 +0530

    2958.cpp added

---
+++ 2958.cpp
@@ -0,0 +1,24 @@
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        
+        unordered_map<int,int>mp;
+        
+        long long ans;
+        int left=0;
+
+        for (int right =0;right<nums.size();right++){
+            
+            mp[nums[right]]++;
+
+            while(mp[nums[right]]>k){
+                mp[nums[left]]--;
+                left++;
+            }
+
+            ans=max(ans, right-left+1);
+        }
+        return ans;
+    }
+};
