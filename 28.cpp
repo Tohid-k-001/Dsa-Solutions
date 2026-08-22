f64ea9ca09cc2e7735e49b79c0832c70ed20109c
Tohid Kazi <155968885+Tohid-k-001@users.noreply.github.com>
2026-08-22 20:22:37 +0530

    Create 28.cpp

---
+++ 28.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n=needle.size();
+        int m=haystack.size();
+
+        for(int i=0; i<=m-n ; i++){
+            // for finding the word needle 
+            int j=0;
+            while(j<n && haystack[i+j]==needle[j]){
+                j++;
+            }
+            if(j==n){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
