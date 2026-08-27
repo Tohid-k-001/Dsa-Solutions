5e6a1f14c0550715f6490ca76cf2a206f94ce689
Tohid Kazi <155968885+Tohid-k-001@users.noreply.github.com>
2026-08-27 23:48:40 +0530

    Update 3720.cpp

--- 3720.cpp
+++ 3720.cpp
@@ -5,73 +5,103 @@ public:
 
         int freq[26] = {};
 
-        // Count characters of s
         for (char c : s) {
             freq[c - 'a']++;
         }
 
-        // Try to match target from left to right
+        string prefix = "";
+
         for (int i = 0; i < n; i++) {
 
-            // If target[i] is available, use it
-            if (freq[target[i] - 'a'] > 0) {
-                freq[target[i] - 'a']--;
+            int x = target[i] - 'a';
+
+            // Case 1: We can keep the same character
+            if (freq[x] > 0) {
+                freq[x]--;
+                prefix += target[i];
+                continue;
             }
-            else {
-                // We cannot continue.
-                // Go backwards and try to make something bigger.
 
-                for (int j = i - 1; j >= 0; j--) {
-
-                    // Return the character used at position j
-                    freq[target[j] - 'a']++;
-
-                    // Find smallest character > target[j]
-                    for (int c = target[j] - 'a' + 1; c < 26; c++) {
-
-                        if (freq[c] > 0) {
-
-                            string ans = target.substr(0, j);
-
-                            // Make this position slightly bigger
-                            ans += char('a' + c);
-                            freq[c]--;
-
-                            // Add remaining characters in sorted order
-                            for (int k = 0; k < 26; k++) {
-                                while (freq[k] > 0) {
-                                    ans += char('a' + k);
-                                    freq[k]--;
-                                }
-                            }
-
-                            return ans;
-                        }
-                    }
-                }
-
-                return "";
-            }
-        }
-
-        // target itself can be formed.
-        // But we need something STRICTLY greater.
-        for (int j = n - 1; j >= 0; j--) {
-
-            // Return the character used at j
-            freq[target[j] - 'a']++;
-
-            // Find smallest character greater than target[j]
-            for (int c = target[j] - 'a' + 1; c < 26; c++) {
+            // Case 2: We cannot keep target[i].
+            // Try making the current position bigger.
+            for (int c = x + 1; c < 26; c++) {
 
                 if (freq[c] > 0) {
 
-                    string ans = target.substr(0, j);
-
+                    string ans = prefix;
                     ans += char('a' + c);
+
                     freq[c]--;
 
-                    // Smallest possible suffix
+                    for (int k = 0; k < 26; k++) {
+                        while (freq[k] > 0) {
+                            ans += char('a' + k);
+                            freq[k]--;
+                        }
+                    }
+
+                    return ans;
+                }
+            }
+
+            // Can't make current position bigger.
+            // We need to backtrack.
+            while (!prefix.empty()) {
+
+                int j = prefix.size() - 1;
+
+                // Put the previous character back
+                freq[prefix[j] - 'a']++;
+
+                prefix.pop_back();
+
+                int prev = target[j] - 'a';
+
+                // Find smallest character > target[j]
+                for (int c = prev + 1; c < 26; c++) {
+
+                    if (freq[c] > 0) {
+
+                        string ans = prefix;
+                        ans += char('a' + c);
+
+                        freq[c]--;
+
+                        for (int k = 0; k < 26; k++) {
+                            while (freq[k] > 0) {
+                                ans += char('a' + k);
+                                freq[k]--;
+                            }
+                        }
+
+                        return ans;
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        // target itself was possible.
+        // We still need something greater.
+        while (!prefix.empty()) {
+
+            int j = prefix.size() - 1;
+
+            freq[prefix[j] - 'a']++;
+            prefix.pop_back();
+
+            int x = target[j] - 'a';
+
+            for (int c = x + 1; c < 26; c++) {
+
+                if (freq[c] > 0) {
+
+                    string ans = prefix;
+                    ans += char('a' + c);
+
+                    freq[c]--;
+
                     for (int k = 0; k < 26; k++) {
                         while (freq[k] > 0) {
                             ans += char('a' + k);
