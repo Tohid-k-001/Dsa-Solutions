875630e314c20b9b28978a34e2879f0c81853a5b
Tohid Kazi <155968885+Tohid-k-001@users.noreply.github.com>
2026-09-04 13:02:14 +0530

    Create 3876.cpp

---
+++ 3876.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minOdd = INT_MAX;
+
+        // Find the smallest odd number
+        for (int x : nums1) {
+            if (x % 2 == 1) {
+                minOdd = min(minOdd, x);
+            }
+        }
+
+        // No odd numbers -> all numbers are even
+        if (minOdd == INT_MAX) {
+            return true;
+        }
+
+        // If an even number is smaller than the smallest odd,
+        // we cannot make all numbers have the same parity.
+        for (int x : nums1) {
+            if (x % 2 == 0 && x < minOdd) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
